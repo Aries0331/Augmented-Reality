ce8136e602cef6943cb0e3e99f96c3f294182377
Aries0331 <jinzhu@ualberta.ca>
2017-02-04 13:18:44 -0700

    missing requested error

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VoiceControl
-Voice control using Unity for CMPUT 414
+Voice control using Unity for CMPUT 414   
+
+Reference tutorial from:   
+https://www.youtube.com/watch?v=Ok_3klyGueI&t=31s    
+https://www.youtube.com/watch?v=HwT6QyOA80E   
--- test1/Assets/Recognition.cs
+++ test1/Assets/Recognition.cs
@@ -1,1 +1,51 @@
-﻿// Video tutorial from https://www.youtube.com/watch?v=HwT6QyOA80E  using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.Windows.Speech; using System.Linq;  public class Recognition : MonoBehaviour {  	// create an object called keywordRecognizer 	KeywordRecognizer keywordRecognizer; 	// use string for recognition and action for call back 	// create a dictionary coutaing words and actions according to the words 	Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();  	// create keywordrecognition and set it up  	// Use this for initialization 	void Start () { 		//initialize stuff 		keywords.Add("go", () => 			{ 				GoCalled(); 			} );  		keywordRecognizer = new KeywordRecognizer (keywords.Keys.ToArray ()); 		keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPharseRecognized; 		keywordRecognizer.Start (); 	}  	void KeywordRecognizerOnPharseRecognized(PhraseRecognizedEventArgs args) 	{ 		System.Action keywordAction;  		if(keywords.TryGetValue(args.text, out keywordAction)) // args.text is the word we just said 		{ 			keywordAction.Invoke (); 		} 	}  	void GoCalled() { 		print ("You just said GO"); 	}  	// Update is called once per frame 	void Update () {  	}  }  
+﻿// Video tutorial from https://www.youtube.com/watch?v=HwT6QyOA80E
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class Recognition : MonoBehaviour {
+
+	// create an object called keywordRecognizer
+	KeywordRecognizer keywordRecognizer;
+	// use string for recognition and action for call back
+	// create a dictionary coutaing words and actions according to the words
+	Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+	// create keywordrecognition and set it up 
+	// Use this for initialization
+	void Start () {
+		//initialize stuff
+		keywords.Add("go", () =>
+			{
+				GoCalled();
+			} );
+
+		keywordRecognizer = new KeywordRecognizer (keywords.Keys.ToArray ());
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPharseRecognized;
+		keywordRecognizer.Start ();
+	}
+
+	void KeywordRecognizerOnPharseRecognized(PhraseRecognizedEventArgs args)
+	{
+		System.Action keywordAction;
+
+		if(keywords.TryGetValue(args.text, out keywordAction)) // args.text is the word we just said
+		{
+			keywordAction.Invoke ();
+		}
+	}
+
+	void GoCalled() {
+		print ("You just said GO");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
+
